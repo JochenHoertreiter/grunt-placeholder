ba7c231a9225142b9e72f9aa24665b339ac83c58
Jochen Hoertreiter <Jochen.Hoertreiter@msg-systems.com>
2015-06-15 12:38:20 +0200

    Initial plugin version

---
+++ README.md
@@ -0,0 +1,130 @@
+
+# grunt-placeholder
+
+Grunt Task for replacing placeholders in source files with values from a configuration file
+
+<p/>
+<img src="https://nodei.co/npm/grunt-placeholder.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/JochenHoertreiter/grunt-placeholder.png" alt=""/>
+
+
+## Getting Started
+
+This plugin requires Grunt `~0.4.0`
+
+If you haven't used [Grunt](http://gruntjs.com/)
+before, be sure to check out the [Getting
+Started](http://gruntjs.com/getting-started) guide, as it explains how
+to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as
+install and use Grunt plugins. Once you're familiar with that process,
+you may install this plugin with this command:
+
+```shell
+npm install grunt-placeholder --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your
+Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-placeholder');
+```
+
+## Task Options
+
+TODO:
+- `replacer`: (default `null`) the replacer argument for `JSON.stringify()` (second argument).
+- `space`: (default `\t`) the space argument for `JSON.stringify()` (third argument).
+
+- `configFile`: (default `component.yaml`) the configuration file that contains key/values for any replacements.
+- `configType`: (default `yaml`) the type of the configuration file - possible values are `json` or `yaml`.
+- `configEncoding`: (default `utf8`) file encoding for configuration files.
+- `sourceEncoding`: (default `utf8`) file encoding for source files.
+- `placeholderRegex`: (default `/___config\.([a-zA-Z][a-zA-Z0-9]*(?:\.[a-zA-Z][a-zA-Z0-9]*)*)___/g`)
+                      placeholder used by source files in order to replace keys with values from the configuration file.
+:TODO END
+
+## Merge JSON Task
+
+_Run this task with the `grunt placeholder` command._
+
+Task targets, files and options may be specified according to the Grunt
+[Configuring tasks](http://gruntjs.com/configuring-tasks) guide.
+
+## Usage Example
+
+TODO:
+Assuming we have the following types of source JSON files:
+
+- `src/foo/foo-en.json`:
+
+```json
+{
+    "foo": {
+        "title": "The Foo",
+        "name":  "A wonderful component"
+    }
+}
+```
+
+- `src/bar/bar-en.json`:
+
+```json
+{
+    "bar": {
+        "title": "The Bar",
+        "name":  "An even more wonderful component"
+    }
+}
+```
+
+Assuming we want to generate the following destination JSON file:
+
+```json
+{
+    "foo": {
+        "title": "The Foo",
+        "name":  "A wonderful component"
+    },
+    "bar": {
+        "title": "The Bar",
+        "name":  "An even more wonderful component"
+    }
+}
+```
+
+### Single file per target variant
+
+```js
+grunt.initConfig({
+    "merge-json": {
+        "en": {
+            src: [ "src/**/*-en.json" ],
+            dest: "www/en.json"
+        },
+        "de": {
+            src: [ "src/**/*-de.json" ],
+            dest: "www/de.json"
+        }
+    }
+});
+```
+
+### Multiple files per target variant
+
+```js
+grunt.initConfig({
+    "merge-json": {
+        "i18n": {
+            files: {
+                "www/en.json": [ "src/**/*-en.json" ],
+                "www/de.json": [ "src/**/*-de.json" ]
+            }
+        }
+    }
+});
+```
+
+:TODO END
---
+++ tasks/grunt-placeholder.js
@@ -0,0 +1,88 @@
+/*
+ **  grunt-placeholder
+ **  Grunt Task for replacing placeholders in source files with values from a configuration file
+ **  Copyright (c) 2015 Jochen Hoertreiter <jochen.hoertreiter@googlemail.com>
+ **
+ **  Permission is hereby granted, free of charge, to any person obtaining
+ **  a copy of this software and associated documentation files (the
+ **  "Software"), to deal in the Software without restriction, including
+ **  without limitation the rights to use, copy, modify, merge, publish,
+ **  distribute, sublicense, and/or sell copies of the Software, and to
+ **  permit persons to whom the Software is furnished to do so, subject to
+ **  the following conditions:
+ **
+ **  The above copyright notice and this permission notice shall be included
+ **  in all copies or substantial portions of the Software.
+ **
+ **  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ **  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ **  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ **  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ **  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ **  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ **  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/* global module: false */
+module.exports = function (grunt) {
+    /* global require: false */
+    var chalk = require("chalk");
+    var path = require("path");
+    var ducky = require("ducky");
+    var cacheLRU = require("cache-lru");
+
+    grunt.registerMultiTask("grunt-placeholder", "replacing placeholders in source files with values from a configuration file", function () {
+        /*  prepare options  */
+        var options = this.options({
+            configFile: "component.yaml",
+            configType: "yaml", /* json or yaml */
+            configEncoding: "utf8",
+            placeholderRegex: /___config\.([a-zA-Z][a-zA-Z0-9]*(?:\.[a-zA-Z][a-zA-Z0-9]*)*)___/g,
+            sourceEncoding: "utf8"
+        });
+
+        var cache = new cacheLRU();
+        grunt.verbose.writeflags(options, "Options");
+
+        /*  iterate over all src-dest file pairs  */
+        this.files.forEach(function (f) {
+            try {
+                f.src.forEach(function (src) {
+                    var configFile = path.join(f.cwd ? f.cwd : "", path.dirname(src), options.configFile);
+                    var dest = path.join(f.dest, src);
+                    var copyOptions = {};
+
+                    if (grunt.file.exists(configFile)) {
+                        /* copy and replace */
+                        var config = cache.get(configFile);
+                        if (config === undefined) {
+                            config = options.configType === "json" ? grunt.file.readJSON(configFile, {encoding: options.configEncoding})
+                                : grunt.file.readYAML(configFile, {encoding: options.configEncoding});
+                            cache.set(configFile, config);
+                        }
+
+                        copyOptions.encoding = options.sourceEncoding;
+                        copyOptions.process = function (content) {
+                            content = content.replace(options.placeholderRegex, function (m0, m1) {
+                                if (typeof m1 === "undefined")
+                                    throw "Regexp does not contain capture group";
+
+                                /* find the value in the config file and return it as replacement for the key */
+                                var replacement = ducky.select(config, m1);
+                                grunt.log.debug("replacing \"" + m0 + "\" with \"" + replacement + "\"");
+                                return replacement;
+                            });
+                            return content;
+                        };
+                        grunt.log.writeln("File \"" + chalk.green(src) + "\" substituted.");
+                    }
+                    grunt.file.copy(path.join(f.cwd ? f.cwd : "", src), dest, copyOptions);
+                });
+            }
+            catch (e) {
+                grunt.fail.warn(e);
+            }
+        });
+    });
+};
+
